ef23c2bf1e0825b45fbb10bf5c32919afa0d7b05
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2020-11-29 20:32:11 -0800

    adjusted clock positon

--- react-app/src/Components/Clock.jsx
+++ react-app/src/Components/Clock.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from 'react';
+import sun from '../assets/desktop/icon-sun.svg';
+import moon from '../assets/desktop/icon-moon.svg';
 
-const Clock = () => {
-    return (
-        <div>
-            <h1>Clock</h1>
-        </div>
-    )
+const Clock = (props) => {
+   console.log(props)
+
+   let getTime = () => {
+      let d = new Date();
+      let hr = d.getHours();
+      let min = d.getMinutes();
+
+      if (min < 10) {
+         min = '0' + min;
+      }
+
+      let currentTime = hr + ':' + min;
+
+      return currentTime;
+   }
+
+   return (
+      <div className="time">
+         <div className="time__greeting-wrapper">
+            {
+               props.hour >= 5 && props.hour < 12 ? (
+                  <div className="time__greeting">
+                     <img src={sun} alt="sun" className="time__greeting-icon" />
+                     <h3 className="time__greeting-text">Good Morning</h3>
+                  </div>
+               ) : (
+                     <div className="time__greeting">
+                        <img src={moon} alt="moon" className="time__greeting-icon" />
+                        <h3 className="time__greeting-text">Good Evening</h3>
+                     </div>
+                  )
+            }
+         </div>
+         <h1 className="time__stamp">{getTime()}</h1>
+      </div>
+   )
 }
 
 export default Clock
--- react-app/src/Components/Main.jsx
+++ react-app/src/Components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Quote from './Quote';
-// import Clock from './Clock';
+import Clock from './Clock';
 // import TimeZone from './TimeZone';
 import dayBackground from '../assets/mobile/bg-image-daytime.jpg';
 import nightBackground from '../assets/mobile/bg-image-nighttime.jpg';
@@ -9,19 +9,21 @@ class Main extends React.Component {
    constructor(props) {
       super(props)
       this.state = {
-         time: new Date().getHours()
+         hour: new Date().getHours(),
+         time: new Date().toLocaleTimeString(),
       };
    }
 
    render() {
       return (
          <div className="background"
-            style={(this.state.time >= 5 && this.state.time < 12) ?
+            style={(this.state.hour >= 5 && this.state.hour < 12) ?
                { backgroundImage: `url(${dayBackground})` } :
-               (this.state.time >= 12 && this.state.time < 18 ?
-                  { backgroundImage: `url(${nightBackground})` } :
-                  { backgroundImage: `url(${dayBackground})` })} >
-            {this.state.time}
+               (this.state.hour >= 12 && this.state.hour < 18 ?
+                  { backgroundImage: `url(${dayBackground})` } :
+                  { backgroundImage: `url(${nightBackground})` })} >
+
+            <Clock hour={this.state.hour} />
          </div>
       )
    }
